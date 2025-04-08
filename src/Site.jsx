93a23c1cdd6fb9d1a14b1d2202d71c1b61537a86
CaioViveiros <caio.soares@sptech.school>
2025-04-08 15:29:25 -0300

    Feat: Atualizações no site institucional

--- src/Site.jsx
+++ src/Site.jsx
@@ -1,30 +1,30 @@
-import { MoveRight } from "lucide-react";
+import { ChevronUp, MoveRight } from "lucide-react";
 import { Header } from "./components/Header";
 
 export function Site() {
     return (
         <div className="bg-[#F0F6F9] max-w-screen h-full">
             <Header />
-            <div className="flex pt-28">
+            <div className="flex pt-64" id="home">
                 <div className="ml-32 w-[520px] flex flex-col gap-5">
                     <h2 className="text-5xl font-bold">Transforme a gestão da sua <span className="text-[#0292B7]">biblioteca</span> com tecnologia e eficiência.</h2>
                     <p className="text-[#6D6D6D]">Simplifique o controle de empréstimos, organize seu catálogo e acompanhe o histórico de leitura dos alunos de forma prática e eficiente.</p>
                     <p className="flex gap-2 items-center text-[#0292B7] font-bold">Entre em contato <MoveRight className="text-[#0292B7] w-5" /></p>
                 </div>
             </div>
-            <div className=" bg-white py-10 mt-52 flex items-center gap-40">
-                <img src="public/images/lottus_flower.png" alt="Imagem de uma flor de lotus" className="ml-32 rounded-3xl shadow"/>
+            <div className="bg-white py-10 mt-52 flex items-center gap-40 scroll-mt-36" id="sobre">
+                <img src="public/images/lottus_flower.png" alt="Imagem de uma flor de lotus" className="ml-32 rounded-3xl shadow" />
                 <div className="w-96 flex flex-col gap-3">
                     <p className="text-[14px] text-[#0292B7] font-semibold">Sobre Nós</p>
                     <h2 className="text-2xl font-bold">Fluidez, Conhecimento e Inovação</h2>
-                    <p className="text-[14px] text-[#6D6D6D]">Inspirados na flor de lótus, símbolo de resiliência e conhecimento, criamos um sistema inovador para gestão de bibliotecas. 
-                    Com tecnologia moderna e eficiente, ajudamos bibliotecas a florescer no universo do conhecimento.</p>
+                    <p className="text-[14px] text-[#6D6D6D]">Inspirados na flor de lótus, símbolo de resiliência e conhecimento, criamos um sistema inovador para gestão de bibliotecas.
+                        Com tecnologia moderna e eficiente, ajudamos bibliotecas a florescer no universo do conhecimento.</p>
                     <p className="text-[14px] font-semibold text-[#0292B7]">Ler mais</p>
                 </div>
             </div>
             <div className=" bg-white flex flex-col items-center justify-center mt-30 py-20 px-48 gap-10">
                 <h2 className="text-2xl font-semibold mb-10">Conheça os <span className="text-[#0292B7]">princípios</span> que nos guiam</h2>
-                <div className="flex gap-80">
+                <div className="flex gap-68">
                     <div className="w-60 flex flex-col gap-4">
                         <h2 className="font-semibold">Nossa missão</h2>
                         <p className="text-[14px] text-[#6D6D6D]">Facilitar a administração de bibliotecas escolares por meio de tecnologia intuitiva, promovendo a organização do acervo e incentivando o hábito da leitura entre os alunos.</p>
@@ -39,7 +39,15 @@ export function Site() {
                     </div>
                 </div>
             </div>
-            
+            <button
+                onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                className="fixed bottom-10 right-10 bg-white text-[#0292B7] p-2 rounded-full shadow-lg hover:transition duration-300"
+                title="Voltar ao topo"
+            >
+                <ChevronUp />
+            </button>
+
+
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,37 @@ export function Header() {
     return (
         <div className="w-full flex items-center justify-between h-32 pt-10">
             <img src="public/images/logo.svg" alt="Logo da lottus" className="w-16 h-16 ml-32" />
+            
             <ul className="flex gap-20">
-                <li className="text-[14px] cursor-pointer text-[#1F2122]">Home</li>
-                <li className="text-[14px] cursor-pointer text-[#1F2122]">Sobre Nós</li>
-                <li className="text-[14px] cursor-pointer text-[#1F2122]">Funcionalidades</li>
-                <li className="text-[14px] cursor-pointer text-[#1F2122]">Contato</li>
+                <li className="text-[14px] cursor-pointer text-[#1F2122] relative group">
+                    <a href="#home">
+                        Home
+                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#0292B7] transition-all ease-in-out duration-400 group-hover:w-full"></span>
+                    </a>
+                </li>
+                <li className="text-[14px] cursor-pointer text-[#1F2122] relative group">
+                    <a href="#sobre">
+                        Sobre Nós
+                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#0292B7] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                    </a>
+                </li>
+                <li className="text-[14px] cursor-pointer text-[#1F2122] relative group">
+                    <a href="#funcionalidades">
+                        Funcionalidades
+                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#0292B7] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                    </a>
+                </li>
+                <li className="text-[14px] cursor-pointer text-[#1F2122] relative group">
+                    <a href="#contato">
+                        Contato
+                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#0292B7] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                    </a>
+                </li>
             </ul>
-            <button className="mr-32 text-[14px] border-2 font-semibold w-20 h-8 items-center justify-center rounded-full bg-transparent border-[#0292B7] text-[#0292B7] cursor-pointer">Entrar</button>
+
+            <button className="mr-32 text-[14px] border-2 font-semibold w-20 h-8 items-center justify-center rounded-full bg-transparent border-[#0292B7] text-[#0292B7] cursor-pointer">
+                Entrar
+            </button>
         </div>
-    )
+    );
 }
