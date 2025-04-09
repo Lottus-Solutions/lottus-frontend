4f525c285b0c07ccd64230b011f2e086dc096d76
CaioViveiros <caio.soares@sptech.school>
2025-04-09 17:54:59 -0300

    Fix: Ajuste de posicionamento na home

--- src/Site.jsx
+++ src/Site.jsx
@@ -5,14 +5,14 @@ export function Site() {
     return (
         <div className="bg-[#F0F6F9] max-w-screen h-full">
             <Header />
-            <div className="flex pt-64 justify-between" id="home">
-                <div className="ml-32 w-[520px] flex flex-col gap-5">
+            <div className="flex pt-24 px-32 justify-between items-center gap-8" id="home">
+                <div className="w-[520px] h-72 flex justify-center flex-col gap-5">
                     <h2 className="text-5xl font-bold">Transforme a gestão da sua <span className="text-[#0292B7]">biblioteca</span> com tecnologia e eficiência.</h2>
                     <p className="text-[#6D6D6D]">Simplifique o controle de empréstimos, organize seu catálogo e acompanhe o histórico de leitura dos alunos de forma prática e eficiente.</p>
                     <p className="flex gap-2 items-center text-[#0292B7] font-bold">Entre em contato <MoveRight className="text-[#0292B7] w-5" /></p>
                 </div>
                 <video
-                    className="max-w-3xl mr-32 rounded-3xl shadow-lg relative bottom-20"
+                    className="w-2xl rounded-3xl shadow-lg relative bottom-20~flex"
                     loop
                     autoPlay
                     muted
