194a46cd7c723f6c1b08809baf35bb301ee1df4e
sophture <sophia.balboni@sptech.school>
2025-05-17 13:45:26 -0300

    alterando readme

--- src/components/AlunoListItem.jsx
+++ src/components/AlunoListItem.jsx
@@ -6,7 +6,7 @@ export function AlunoListItem() {
     return (
         <div className="flex gap-x-12 border-b-[1px] border-[#727272] pb-8 justify-around items-center">
             <div className="flex flex-col gap-3 w-16">
-                <p className="text-[#727272] text-xs">Matricula</p>
+                <p className="text-[#727272] text-xs">Matrícula</p>
                 <p className="text-xs">012410</p>
             </div>
             <div className="flex flex-col gap-3 w-64">
@@ -22,7 +22,7 @@ export function AlunoListItem() {
             </div>
             <div className="flex flex-col gap-3 w-64">
                 <p className="text-[#727272] text-xs">Livro Atual</p>
-                <p className="text-xs">Harry Potter e a pedra filosofal</p>
+                <p className="text-xs">Harry Potter e a Pedra Filosofal</p>
             </div>
             
             <BotaoPrincipal nome="Acessar Perfil" />
